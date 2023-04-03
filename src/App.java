bec14fb1a4cac5b959c73f6de3c1646a5b707a27
Pedro Silva <pedrosilva18desenvolvedor@gmail.com>
2023-04-03 20:44:14 -0300

    Primeiro commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner teclado = new Scanner (System.in);
+        int numero = teclado.nextInt();
+
+        System.out.println( "O numero informado foi [" + numero +"]");
+
+
+
+    }
+}
