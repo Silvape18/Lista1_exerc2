a661fe581ac115bad9c999c9cd6bbca71767e9ce
Pedro Silva <pedrosilva18desenvolvedor@gmail.com>
2023-04-03 20:56:57 -0300

    Lista1_exerc2

--- src/App.java
+++ src/App.java
@@ -4,7 +4,9 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         Scanner teclado = new Scanner (System.in);
+        System.out.print("entre com um numero: ");
         int numero = teclado.nextInt();
+
 
         System.out.println( "O numero informado foi [" + numero +"]");
 
